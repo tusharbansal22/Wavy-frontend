886bf05322beef0b8f6fc3faa2724247b047c44b
tusharb <tusharbansal@iitbhilai.ac.in>
2023-06-22 22:43:02 +0530

    ui fixes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -9,7 +9,7 @@ function Dashboard(){
     navigate('/register' , {replace: true});
   }
   return <div>
-    <div  className="block  p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 bg-gray-800 border-gray-700 hover:bg-gray-700 ">
+    <div  className="block  p-6  border border-gray-200 rounded-lg shadow hover:bg-gray-100 bg-slate-800 border-gray-700 hover:bg-gray-700 ">
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-white">Hey! {location.state.email}</h5>
     
 </div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -33,9 +33,9 @@ function Login(){
 
 
   }}>
-  <label htmlFor="email" className=" text-sm text-gray-500 text-gray-400 duration-300 transform -translate-y-0 scale-75 top-2 z-10 origin-[0] bg-white bg-gray-900 px-2  peer-focus:text-blue-600 peer-focus:dark:text-blue-500    left-1">Email</label>
+  <label htmlFor="email" className=" text-sm text-gray-500 text-gray-400 duration-300 transform -translate-y-0 scale-75 top-2 z-10 origin-[0]  bg-slate-900 px-2  peer-focus:text-blue-600 peer-focus:dark:text-blue-500    left-1">Email</label>
   <input type="email" id="email" className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"/>
-  <label  htmlFor= "password" className=" text-sm text-gray-500 text-gray-400 duration-300 transform -translate-y-0 scale-75 top-2 z-10 origin-[0] bg-white bg-gray-900 px-2  left-1">Password</label>
+  <label  htmlFor= "password" className=" text-sm text-gray-500 text-gray-400 duration-300 transform -translate-y-0 scale-75 top-2 z-10 origin-[0] bg-slate-900 px-2  left-1">Password</label>
   <input type="password" id="password" className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"/>
   <button type="submit" className="bg-teal-400 hover:bg-gray-100 text-gray-800 font-semibold  my-6 py-2 px-4 border border-gray-400 rounded shadow"  > Login</button>
   </form>
